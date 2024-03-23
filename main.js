c1ebcf51cd3968bc7dee154d4656708b6f0f5e05
JimmyIstWeicher <143940482+JimmyIstWeicher@users.noreply.github.com>
2024-03-23 17:48:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,25 +45,6 @@ const safetySettings = [
   },
 ];
 
-const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-  ];
-
 
 // DOM-Elemente
 const form = document.querySelector("form");
